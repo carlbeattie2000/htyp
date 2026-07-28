14aab8398363500bdb4b783467e1098e3e29dd31
Carl Beattie <contact.carlbeattie@gmail.com>
2026-07-28 05:53:45 +0100

    feat: export some core types

--- lib/htyp.ts
+++ lib/htyp.ts
@@ -1,5 +1,9 @@
 import Htyp from "./core/Htyp";
 
+export type { default as Htyp } from "./core/Htyp";
+export type { HtypRequestConfig } from "./types/config";
+export type { HtypResponse } from "./types/response";
+
 const htyp = new Htyp();
 
 export default htyp;
